393af85b762405f2731e4e44c1bf345935cceba0
Young Bin Na <whybee0405@gmail.com>
2025-06-02 16:42:19 +0200

    Initial project setup

---
+++ main.py
@@ -0,0 +1,11 @@
+print(
+    '''
+    Welcome to the CLI To Do Task App created by Young Bin Na.
+    
+    Please choose from the menu:
+    1. Login
+    2. Forgot Password
+    
+    If you forgot your username then please contact your administrator.
+    '''
+)
